3a7fb4696e8955dbe98d8353a36037c858a328c3
Anneli <anneli@svaja.se>
2016-02-27 10:02:33 +0100

    New input menu and navigationbar

--- app/src/main/java/com/example/idnert/kol_app/Controller.java
+++ app/src/main/java/com/example/idnert/kol_app/Controller.java
@@ -4,4 +4,10 @@ package com.example.idnert.kol_app;
  * Created by idnert on 2016-02-25.
  */
 public class Controller {
+
+    private LayoutPersonalPref layoutPersonalPref;
+
+    public Controller(LayoutPersonalPref layoutPersonalPref) {
+        this.layoutPersonalPref = layoutPersonalPref;
+    }
 }
