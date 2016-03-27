7653b194d3c1863c7600a69c659483d1a61bab44
Anneli <anneli@svaja.se>
2016-03-27 12:05:09 +0200

    Trying to fix the cardview to be updatade with the db, it did'nt work

--- app/src/main/java/com/example/idnert/kol_app/RecyclerViewActivity.java
+++ app/src/main/java/com/example/idnert/kol_app/RecyclerViewActivity.java
@@ -1,41 +1,7 @@
 package com.example.idnert.kol_app;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class RecyclerViewActivity extends Activity {
-     List<Exercis> exercises;
-     RecyclerView rv;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_recycler_view);
-
-        rv=(RecyclerView)findViewById(R.id.rv);
-        LinearLayoutManager llm= new LinearLayoutManager(this);
-        rv.setLayoutManager(llm);
-        rv.setHasFixedSize(true);
-
-        initializeData();
-        initializeAdapter();
-    }
-
-    private void initializeData() {
-        exercises = new ArrayList<>();
-        exercises.add(new Exercis("Promenad", "en mysig promenad, lyssna till det som händer runt dig...", R.drawable.walk));
-        exercises.add(new Exercis("Dansa", "Ta en sväng om...", R.drawable.seniors_dance));
-        exercises.add(new Exercis("Meditation", "En lugn och stilla minut för att öka närvaron", R.drawable.breading));
-    }
-
-    private void initializeAdapter() {
-        RVAadapter adapter = new RVAadapter(exercises);
-        rv.setAdapter(adapter);
-    }
+/**
+ * Created by idnert on 2016-03-26.
+ */
+public class RecyclerViewActivity {
 }
